d079e1bd20b1bf0f067980e721057cc8207c3512
suhrobfrontend <sukhrobfrontend@gmail.com>
2022-12-02 16:42:46 +0500

    app updated

--- js/app.js
+++ js/app.js
@@ -1,13 +1,58 @@
-var elHeading = document.querySelector("#heading");
-var dollar = 11200;
-var yevro = 11500;
-var jami = dollar * 500 + dollar * 250 + yevro * 120;
-var summa = +prompt("Qancha mablag'ingiz bor?");
+// var elHeading = document.querySelector("#heading");
+// var dollar = 11200;
+// var yevro = 11500;
+// var jami = dollar * 500 + dollar * 250 + yevro * 120;
+// var summa = +prompt("Qancha mablag'ingiz bor?");
 
-if (summa >= jami) {
-  elHeading.textContent = "Oq yo'l Jonibek aka!";
-} else if (summa < jami) {
-  elHeading.textContent = "Jonibek aka uyda o'tiring!";
-} else {
-  elHeading.textContent = "Raqam kiriting!";
-}
+// if (summa >= jami) {
+//   elHeading.textContent = "Oq yo'l Jonibek aka!";
+// } else if (summa < jami) {
+//   elHeading.textContent = "Jonibek aka uyda o'tiring!";
+// } else {
+//   elHeading.textContent = "Raqam kiriting!";
+// }
+
+// // STUDENT MASHQI
+// var elHeading = document.querySelector("#heading");
+// var question1 = prompt("Ismingizni kriting:");
+// var question2 = +prompt("Balingizni kiriting:");
+// var question3 = prompt("Tankangiz bormi?:");
+
+// var hundred = 100;
+// var seventy = 70;
+// var fourty = 40;
+
+// if (question2 > hundred) {
+//   alert("Maksimal 100 ball kiriting");
+// } else if (question2 >= seventy) {
+//   elHeading.textContent = `Hurmat li ${question1}, sizning balingiz ${question2}, siz o'tdingiz, tabriklaymiz!`;
+// } else if (question2 < seventy && question2 >= fourty && question3 == "ha") {
+//   elHeading.textContent = `Hurmat li ${question1}, sizning balingiz ${question2}, siz o'tdingiz! O'zingiz ham harakat qiling`;
+// } else {
+//   elHeading.textContent = `Afsus keyingi safar o'tasiz:(`;
+// }
+
+// _________________________
+
+// Currency converter Money Gram
+var elForm = document.querySelector("[data-form]");
+var elInput = document.querySelector("[data-input]");
+var elSelect = document.querySelector("[data-select]");
+var elButton = document.querySelector("[data-btn]");
+var elHeading = document.querySelector("[data-heading]");
+// valutalar
+var rub = 181;
+var dollar = 11257;
+var euro = 11830;
+
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  if (elSelect.value === "rub") {
+    elHeading.textContent = `${Math.round(elInput.value / rub)} "rubl" pulingiz bor`;
+  } else if (elSelect.value === "dollar") {
+    elHeading.textContent = `${Math.round(elInput.value / dollar)} "dollar" pulingiz bor`;
+  } else {
+    elHeading.textContent = `${Math.round(elInput.value / euro)} "euro" pulingiz bor`;
+  }
+});
